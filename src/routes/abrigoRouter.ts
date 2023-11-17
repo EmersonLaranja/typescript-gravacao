2ab6080926f3ac1731aab14ed3eb8fe0b0cec7c4
EmersonLaranja <emersonlaranja@gmail.com>
2023-11-17 17:00:42 -0300

    5.3

---
+++ src/routes/abrigoRouter.ts
@@ -0,0 +1,23 @@
+import express, { RequestHandler } from "express";
+import { AppDataSource } from "../config/dataSource";
+import AbrigoController from "../controller/AbrigoController";
+import AbrigoRepository from "../repositories/AbrigoRepository";
+const router = express.Router();
+const abrigoRepository = new AbrigoRepository(
+  AppDataSource.getRepository("AbrigoEntity")
+);
+const abrigoController = new AbrigoController(abrigoRepository);
+
+router.post("/", (req, res) => abrigoController.criaAbrigo(req, res));
+
+router.get("/", (req, res) => abrigoController.listaAbrigos(req, res));
+
+router.put("/:id", (req, res) => abrigoController.atualizaAbrigo(req, res));
+
+router.delete("/:id", (req, res) => abrigoController.deletaAbrigo(req, res));
+
+router.patch("/:id", (req, res) =>
+  abrigoController.atualizaEnderecoAbrigo(req, res)
+);
+
+export default router;
